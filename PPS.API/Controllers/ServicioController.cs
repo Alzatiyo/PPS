0640f460e1acd1ae5761bdf5429b5a92652d7b01
salzate <alzatiyo10@gmail.com>
2023-10-21 11:45:23 -0500

    Se hacen ajustes finales

---
+++ PPS.API/Controllers/ServicioController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PPS.API.Data;
+using PPS.Shared.Entities;
+
+namespace PPS.API.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("api/Servicio")]
+    public class ServicioController : Controller
+    {
+        private readonly DataContext _context;
+        public ServicioController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Servicios.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var servicio = await _context.Servicios.FirstOrDefaultAsync(m => m.Id == id);
+            if (servicio == null)
+                return NotFound();
+            else
+                return Ok(servicio);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Servicio servicio)
+        {
+            _context.Add(servicio);
+            await _context.SaveChangesAsync();
+            return Ok(servicio);
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put(Servicio servicio)
+        {
+            _context.Update(servicio);
+            await _context.SaveChangesAsync();
+            return Ok(servicio);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var servicio = await _context.Servicios.Where(m => m.Id == id).ExecuteDeleteAsync();
+
+            if (servicio == 0)
+                return NotFound();
+            else
+                return NoContent();
+        }
+    }
+}
---
+++ PPS.API/Controllers/VehiculosController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PPS.API.Data;
+using PPS.Shared.Entities;
+
+namespace PPS.API.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("api/Vehiculos")]
+    public class VehiculosController : Controller
+    {
+        private readonly DataContext _context;
+        public VehiculosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Vehiculos.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var vehiculo = await _context.Vehiculos.FirstOrDefaultAsync(m => m.Id == id);
+            if (vehiculo == null)
+                return NotFound();
+            else
+                return Ok(vehiculo);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Vehiculo vehiculo)
+        {
+            _context.Add(vehiculo);
+            await _context.SaveChangesAsync();
+            return Ok(vehiculo);
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put(Vehiculo vehiculo)
+        {
+            _context.Update(vehiculo);
+            await _context.SaveChangesAsync();
+            return Ok(vehiculo);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var vehiculo = await _context.Vehiculos.Where(m => m.Id == id).ExecuteDeleteAsync();
+
+            if (vehiculo == 0)
+                return NotFound();
+            else
+                return NoContent();
+        }
+    }
+}
